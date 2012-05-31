069819aef9f6b4fab50a74bbf8487954932d5af2
Peter Donald <peter@realityforge.org>
2012-05-31 11:17:49 +1000

    Read the long description from the correctly named file

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "Noan Kantrowitz"
 maintainer_email "nkantrowitz@crypticstudios.com"
 license          "Apache 2.0"
 description      "Install and configure the collectd monitoring daemon and plugins"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.1"
 supports         "ubuntu"
