7eb6581d65b577f14498aa7d0f40d717aec9a36c
Peter Donald <peter@realityforge.org>
2012-12-03 16:23:11 +1100

    Whitespace

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ license          "Apache 2.0"
 description      "Install and configure the collectd monitoring daemon and plugins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.5"
+
 supports         "ubuntu"
 depends          "build-essential"
