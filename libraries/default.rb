75c5aa5395962a07b3511ccf444c6da154597a29
Peter Donald <peter@realityforge.org>
2012-12-31 12:39:02 +1100

    Don't use interpolation unless required

--- libraries/default.rb
+++ libraries/default.rb
@@ -19,7 +19,7 @@
 
 def collectd_key(option)
   return option.to_s.split('_').map{|x| x.capitalize}.join() if option.instance_of?(Symbol)
-  "#{option}"
+  option.to_s
 end
 
 def collectd_option(option)
