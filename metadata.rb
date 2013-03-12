fa928f1e5d9791036458fde91cf5418a9d083ea4
Peter Donald <peter@realityforge.org>
2013-03-12 15:43:32 +1100

    Improve the description a whisker

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'collectd'
 maintainer       'Peter Donald'
 maintainer_email 'peter@realityforge.org'
 license          'Apache 2.0'
-description      'Install and configure the collectd monitoring daemon and plugins'
+description      'Install and configure the collectd monitoring daemon and plugin configurations'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.5'
 
