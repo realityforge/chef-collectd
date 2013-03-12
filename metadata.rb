35eb907683ff43bc9b8b277e22305bb913367a85
Peter Donald <peter@realityforge.org>
2013-03-12 15:42:46 +1100

    Correct the maintainer details

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'collectd'
-maintainer       'Noan Kantrowitz'
-maintainer_email 'nkantrowitz@crypticstudios.com'
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
 license          'Apache 2.0'
 description      'Install and configure the collectd monitoring daemon and plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
