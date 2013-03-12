5353077686f1476dae14c50541c9a6651c353b15
Peter Donald <peter@realityforge.org>
2013-03-12 15:42:04 +1100

    Replace " with ' quotes

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             "collectd"
-maintainer       "Noan Kantrowitz"
-maintainer_email "nkantrowitz@crypticstudios.com"
-license          "Apache 2.0"
-description      "Install and configure the collectd monitoring daemon and plugins"
+name             'collectd'
+maintainer       'Noan Kantrowitz'
+maintainer_email 'nkantrowitz@crypticstudios.com'
+license          'Apache 2.0'
+description      'Install and configure the collectd monitoring daemon and plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.5"
+version          '1.1.5'
 
-supports         "ubuntu"
-depends          "build-essential"
+supports         'ubuntu'
+depends          'build-essential'
